fefebcee2ae9a1d70a16983829a5250fe25cd88d
eduran19 <56426468+eduran19@users.noreply.github.com>
2020-12-30 20:57:13 +0300

    Update Main.java

--- ProjectEuler 1/src/com/company/Main.java
+++ ProjectEuler 1/src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
        int toplam =0;
-       //ilk örnek
+      
         for(int i =1;i<1000;i++){
             if(i%3 ==0 || i%5 ==0){
                toplam = toplam + i;
